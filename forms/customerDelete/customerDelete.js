75823ce531f8306f8640305d024d6cc71f3e9287
Laura Higueras <lhi72634@creighton.edu>
2021-03-22 22:09:06 -0500

    Update customerDelete.js

--- forms/customerDelete/customerDelete.js
+++ forms/customerDelete/customerDelete.js
@@ -19,7 +19,7 @@ customerDelete.onshow=function(){
     else if (found == true) {
       query = "DELETE FROM customer WHERE name = '" + customerName + "'"      
       req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
-      if (req.status == 200) //transit worked.
+      if (req.status == 200) 
             if (req.responseText == 500)    
                 lblMessage2.value = `You have successfully deleted the customer named ${customerName}`
             else
